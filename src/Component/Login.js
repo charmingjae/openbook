0127cd7619d822029a7dad48ba9e4e54ef23f6b8
chaminjae <beingeskimo@gmail.com>
2020-09-09 21:58:33 +0900

    Login

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -4,12 +4,41 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+import axios from "axios";
+
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       page: "Main",
     };
+  }
+
+  // * 로그인 함수
+  doLogin = async (e) => {
+    e.preventDefault();
+    const res = await axios("http://localhost:4000/add/login", {
+      method: "POST",
+      data: this.state,
+      headers: new Headers(),
+    });
+
+    if (res.data) {
+      console.log("Chk suc : ", res.data.suc);
+      if (res.data.suc) {
+      } else {
+        alert("아이디와 비밀번호를 확인하세요.");
+      }
+    }
+  };
+
+  // * id 상태 변경 함수
+  idUpdate(e) {
+    this.setState({ userid: e.target.value });
+  }
+  // * pw 상태 변경 함수
+  pwUpdate(e) {
+    this.setState({ password: e.target.value });
   }
 
   selectRegister = async () => {
@@ -24,12 +53,13 @@ class Login extends Component {
       <>
         <div className="wrapper">
           <Typography className="shwLogin">로그인</Typography>
-          <form noValidate autoComplete="off">
+          <form method="POST" onSubmit={this.doLogin}>
             <TextField
               label="아읻이"
               variant="outlined"
               color="primary"
               className="txtID"
+              onChange={(e) => this.idUpdate(e)}
             />
             <TextField
               label="팻으어드"
@@ -37,8 +67,14 @@ class Login extends Component {
               color="primary"
               className="txtPW"
               type="password"
+              onChange={(e) => this.pwUpdate(e)}
             />
-            <Button className="subLogin" variant="contained" color="primary">
+            <Button
+              className="subLogin"
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
               로그인
             </Button>
             <br />
